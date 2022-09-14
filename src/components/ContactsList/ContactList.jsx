606fbb8614763e48ef9ca8e024ecc07048b84466
IlliaKlason <105161428+IlliaKlason@users.noreply.github.com>
2022-09-14 20:49:19 +0300

    Summary

---
+++ src/components/ContactsList/ContactList.jsx
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+import { Box } from 'components/Box/Box';
+import { List, ListItem } from './ContactList.styled';
+import { Contact } from './Contact/Contact';
+
+export const ContactList = ({ contacts, onDeleteContact }) => (
+  <List>
+    {contacts.map(({ id, name, number }) => (
+      <ListItem key={id}>
+        <Contact
+          id={id}
+          name={name}
+          number={number}
+          onDeleteContact={onDeleteContact}
+        />
+      </ListItem>
+    ))}
+  </List>
+);
+
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ),
+  onDeleteContact: PropTypes.func.isRequired,
+};
